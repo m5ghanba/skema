09147cabb01eb93955ed5b0d966f197da0345816
m5ghanba <m5ghanba@uwaterloo.ca>
2025-10-30 17:41:53 -0700

    Release v0.1.1

--- skema/__init__.py
+++ skema/__init__.py
@@ -2,7 +2,7 @@
 SKeMa: Satellite-based Kelp Mapping using Semantic Segmentation on Sentinel-2 imagery
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Mohsen Ghanbari"
 __email__ = "mohsenghanbari@uvic.ca"
 __doi__ = "10.57967/hf/6790"
